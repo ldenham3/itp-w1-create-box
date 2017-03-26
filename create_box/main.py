00b30f9e650acb29c59615eadc18ad4cb53b1f23
Leah <ldenham828@gmail.com>
2017-03-26 17:56:40 +0000

    error handling for width and height not greater than or equal to one

--- create_box/main.py
+++ create_box/main.py
@@ -3,12 +3,14 @@
 
 def create_box(height, width, character):
     box = ""
-    for x in range(height):
-        for y in range(width):
-            box += character
-        box += "\n"
-    return box
-
+    if width >= 1 & height >= 1:
+        for x in range(height):
+            for y in range(width):
+                box += character
+            box += "\n"
+        return box
+    else:
+        return "Need a valid width and height"
 
 if __name__ == '__main__':
     create_box(3, 4, '*')
